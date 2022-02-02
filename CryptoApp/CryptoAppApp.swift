fc9d82e5b8b9498c112379692587d27c82726461
skizzy8535 <smirkiiiv@gmail.com>
2022-02-02 15:36:31 +0800

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by 林祐辰 on 2022/2/2.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
